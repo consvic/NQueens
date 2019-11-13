ae91a6a43b8bc7b02c8b94b6e6b2001cd30db7e3
Constanza Lievanos <cocoy_shuffle@me.com>
2019-11-12 21:29:44 -0600

    added to previos solution integration with database and some cleanup

--- main.py
+++ main.py
@@ -1,17 +1,76 @@
+import sqlalchemy
+from sqlalchemy.sql import select
+from sqlalchemy import Column, Integer, ARRAY
+
+connection_string = 'postgresql://user:pass@postgresql/mydatabase'
+
+db = sqlalchemy.create_engine(connection_string)  
+engine = db.connect() 
+meta = sqlalchemy.MetaData(engine)
+meta.reflect(bind=engine)
+
 ''' N-Queen problem with recursion (backtracking) '''
 N = 8
 k = 1
+
+solutions = []
 
 ''' This function is to print a board with the places queens '''
 def printBoard(board):
     global k
     print(k, ")\n")
-    k = k + 1
     for row in range(N):
         for column in range(N):
             print(board[row][column], end = " ")
         print("\n")
     print("\n")
+
+''' This function returns the number of solutions found,
+need to run solveNQueenProblem first to proper show the result'''
+def foundSolutions():
+    global k
+    return k - 1
+
+''' This function prepares the data for the DB'''
+def transformForDB(board):
+    simpleBoardFormat = []
+    for row in range(N):
+        for column in range(N):
+            if board[row][column] == 1:
+                simpleBoardFormat.append(column)
+
+    solutions.append(simpleBoardFormat)
+
+''' This functions stores the data in the DB only if there are solutions'''
+def storeDataInDB():
+    if len(solutions) > 0:
+        record = sqlalchemy.table("queensolutions",
+                                    Column("solution_index", Integer),
+                                    Column("number_queens", Integer),
+                                    Column("solution", ARRAY(Integer)))
+
+        for num, solution in enumerate(solutions, start=1):
+            statement = record.insert().values(
+                solution_index = num,
+                number_queens = N,
+                solution = solution,
+            )
+            engine.execute(statement)
+
+''' This function ugly prints all the table entries '''
+def printTable():
+    table = meta.tables["queensolutions"]
+    resultSelect = engine.execute(select([table.c.solution_index, table.c.number_queens, table.c.solution]))
+    rows = resultSelect.fetchall()
+    print(rows)
+
+''' This function fetches for entries for current N'''
+def fetchForNQueenTableEntries():
+    table = meta.tables["queensolutions"]
+    resultSelectNQueen = engine.execute(select([table.c.number_queens]).where(table.c.number_queens == N))
+    rows = resultSelectNQueen.fetchall()
+
+    return rows
 
 ''' This function is to check if a queen can be added to our board
 in a particular row and column (since we are placing queens from left to right
@@ -30,7 +89,7 @@ def isValidSpot(board, row, column):
     columnPosition = column
 
     while rowPosition >= 0 and columnPosition >= 0:
-        if (board[rowPosition][columnPosition]):
+        if board[rowPosition][columnPosition]:
             isValid = False
             return isValid
         rowPosition -= 1
@@ -41,7 +100,7 @@ def isValidSpot(board, row, column):
     columnPosition = column
 
     while rowPosition < N and columnPosition >= 0:
-        if (board[rowPosition][columnPosition]):
+        if board[rowPosition][columnPosition]:
             isValid = False
             return isValid
         rowPosition += 1
@@ -52,14 +111,17 @@ def isValidSpot(board, row, column):
 def solveNQueenProblem(board, column):
     res = False
 
-    ''' Break recursion if we found a solution '''
-    if (column == N):
+    ''' Break recursion if we found a solution we print result and transform the data'''
+    if column == N:
+        transformForDB(board)
         printBoard(board)
+        global k
+        k = k + 1
         res = True
         return res
 
     for index in range(N):
-        if (isValidSpot(board, index, column)):
+        if isValidSpot(board, index, column):
             ''' 1 is our Queen '''
             board[index][column] = 1
 
@@ -74,7 +136,17 @@ def main():
     board = [[0 for i in range(N)]
                 for j in range(N)]
 
-    if (solveNQueenProblem(board, 0) == False):
+    if solveNQueenProblem(board, 0) == False:
         print("Sorry we could not find a solution")
+        return
+
+    entries = fetchForNQueenTableEntries()
+
+    if len(entries) > 0:
+        print(f"No need to store we already have solutions for {N} Queen")
+        return
+    
+    storeDataInDB()
+    printTable()
 
 main()
